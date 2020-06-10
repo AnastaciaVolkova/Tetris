71afe45e450f1ae84a2423fb347b46eaa602fea4
AnastaciaVolkova <polarbolide@gmail.com>
2020-06-10 22:47:14 +0200

    docs: Add commentaries to classes Commands and Controller

--- src/controller.hpp
+++ src/controller.hpp
@@ -4,16 +4,33 @@
 #include "model.hpp"
 #include "view.hpp"
 
+//! Class contains user commands.
 enum class Commands { kLeft, kRight, kRotate, kRotate_ws, kDrop, kNone, kExit };
 
+//! Class takes over interacting between game and its graphical representation.
 class Controller {
 public:
+  //! Constructor
+  /*!
+    \param [in,out] model reference to model of game
+    \param [in,out] view reference to graphical representation of view
+  */
   Controller(Model &model, View &view) : model_(model), view_(view) {}
+
+  //! Game loop
+  /*!
+    \param [in] target_frame_duration duration of game loop (game frame)
+  */
   void Run(size_t target_frame_duration);
 
 private:
   Model &model_;
   View &view_;
+  //! Get user command from keyboard.
+  /*!
+    \param [in] target_frame_duration duration of game loop (game frame)
+    \return Key code
+  */
   Commands Input(size_t target_frame_duration);
 };
 #endif
