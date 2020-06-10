a4944bc50bdbd0ff7d948b0c8d976294c341e2ba
AnastaciaVolkova <polarbolide@gmail.com>
2020-06-11 01:08:49 +0200

    feat: Change window and grid size

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,10 @@ using namespace std;
 int main() {
   constexpr size_t kFramesPerSecond{60};
   constexpr size_t kMsPerFrame{1000 / kFramesPerSecond};
-  constexpr size_t kScreenWidth{640};
-  constexpr size_t kScreenHeight{640};
-  constexpr size_t kGridWidth{32};
-  constexpr size_t kGridHeight{32};
+  constexpr size_t kScreenWidth{800};
+  constexpr size_t kScreenHeight{kScreenWidth * 2};
+  constexpr size_t kGridWidth{kScreenWidth / 10};
+  constexpr size_t kGridHeight{kGridWidth};
 
   View view(kScreenWidth, kScreenHeight, kGridWidth, kGridHeight);
   Model model;
