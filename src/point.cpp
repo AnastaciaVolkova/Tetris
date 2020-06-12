5fa26431447a6c58a3de8d317d3ca9eb9b1723fd
AnastaciaVolkova <polarbolide@gmail.com>
2020-06-12 20:49:47 +0200

    feat: Add color to point

--- src/point.cpp
+++ src/point.cpp
@@ -1,7 +1,7 @@
 #include "point.hpp"
 
 Point::Point() : x(0), y(0){};
-Point::Point(int xx, int yy) : x(xx), y(yy) {}
+Point::Point(int xx, int yy, Color cc) : x(xx), y(yy), color(cc) {}
 Point &Point::operator+=(const Point &ot) {
   x += ot.x;
   y += ot.y;
--- src/point.hpp
+++ src/point.hpp
@@ -2,13 +2,20 @@
 #define _POINT_HPP_
 
 //! Contains figure color
-enum class Color { BROWN, YELLOW };
+enum class Color { BLACK, BROWN, YELLOW };
 
 //! Struct Point implements operations with points
 struct Point {
-  int x, y; // x,y coordinates
+  Color color; // Color of the point.
+  int x, y;    // x,y coordinates.
   Point();
-  Point(int xx, int yy);
+  //! Constructor
+  /*!
+  \param [in] xx x-coordinate
+  \param [in] yy y-coordinate
+  \param [in] cc Color
+  */
+  Point(int xx, int yy, Color color = Color::BLACK);
   Point &operator+=(const Point &ot);
   friend Point operator+(const Point &p1, const Point &p2);
   friend Point operator-(const Point &p1, const Point &p2);
