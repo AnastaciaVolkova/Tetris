274e1b06f5b0372b76d7d572efbf7a3af8b97131
AnastaciaVolkova <polarbolide@gmail.com>
2020-06-13 14:26:26 +0200

    feat: Create 2 rotation state figures (left-s, right-s, stick)

---
+++ src/figure2.cpp
@@ -0,0 +1,65 @@
+#include "figure2.hpp"
+using std::make_unique;
+using std::move;
+using std::unique_ptr;
+
+Figure2::Figure2(Point &&p, Color c) : Figure(move(p), c) {
+  state_ = State::k0;
+}
+
+void Figure2::Rotate() {
+  switch (state_) {
+  case State::k0:
+    state_ = State::k90;
+    break;
+  case State::k90:
+    state_ = State::k0;
+    break;
+  default:
+    break;
+  }
+}
+
+void Figure2::RotateCounter() { Rotate(); }
+
+unique_ptr<Figure2> Figure2::make_ls(Point &&p) {
+  unique_ptr<Figure2> fig = make_unique<Figure2>(Figure2(move(p), Color::LIME));
+  fig->forms_[State::k0] = {{0, 0, fig->color_},
+                            {0, 1, fig->color_},
+                            {1, 1, fig->color_},
+                            {1, 2, fig->color_}};
+  fig->forms_[State::k90] = {{1, 1, fig->color_},
+                             {2, 1, fig->color_},
+                             {0, 2, fig->color_},
+                             {1, 2, fig->color_}};
+  return fig;
+}
+
+unique_ptr<Figure2> Figure2::make_rs(Point &&p) {
+  unique_ptr<Figure2> fig =
+      make_unique<Figure2>(Figure2(move(p), Color::GREEN_YELLOW));
+  fig->forms_[State::k0] = {{1, 0, fig->color_},
+                            {0, 1, fig->color_},
+                            {1, 1, fig->color_},
+                            {0, 2, fig->color_}};
+  fig->forms_[State::k90] = {{0, 1, fig->color_},
+                             {1, 1, fig->color_},
+                             {1, 2, fig->color_},
+                             {2, 2, fig->color_}};
+
+  return fig;
+}
+
+unique_ptr<Figure2> Figure2::make_stick(Point &&p) {
+  unique_ptr<Figure2> fig = make_unique<Figure2>(Figure2(move(p), Color::RED));
+  fig->forms_[State::k0] = {{0, 0, fig->color_},
+                            {0, 1, fig->color_},
+                            {0, 2, fig->color_},
+                            {0, 3, fig->color_}};
+  fig->forms_[State::k90] = {{0, 3, fig->color_},
+                             {1, 3, fig->color_},
+                             {2, 3, fig->color_},
+                             {3, 3, fig->color_}};
+
+  return fig;
+}
---
+++ src/figure2.hpp
@@ -0,0 +1,46 @@
+#ifndef _FIGURE2_HPP_
+#define _FIGURE2_HPP_
+#include "figure.hpp"
+#include "point.hpp"
+#include <map>
+#include <memory>
+#include <vector>
+
+//! Class Figure2 represents the game figure with rotation 2 states
+class Figure2 : public Figure {
+private:
+  //! Constructor
+  /*!
+  \param [in] p point initial position of the figure
+  \param [in] c color of the figure
+  */
+  Figure2(Point &&p, Color c);
+
+public:
+  //! Rotate counter clock-wise
+  void RotateCounter() override;
+
+  //! Rotate clock-wise
+  void Rotate() override;
+
+  //! Makes left-s figure
+  /*!
+  \param [in] p point initial position of figure
+  \return Figure2 object, which is initialized as left-s figure
+  */
+  static std::unique_ptr<Figure2> make_ls(Point &&p);
+  //! Make right-s figure
+  /*!
+  \param [in] p point initial position of figure
+  \return Figure2 object, which is initialized as right-s figure
+  */
+  static std::unique_ptr<Figure2> make_rs(Point &&p);
+
+  //! Make stick figure
+  /*!
+  \param [in] p point initial position of figure
+  \return Figure2 object, which is initialized as stick figure
+  */
+  static std::unique_ptr<Figure2> make_stick(Point &&p);
+};
+#endif
